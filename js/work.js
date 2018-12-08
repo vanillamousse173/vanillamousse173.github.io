d1159830ab9580c66ff5353979de6567c2b018e9
vanillamousse173 <vanillamousse173@gmail.com>
2018-12-08 23:24:15 +0800

    js

---
+++ js/work.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	// $("nav").load("menu.html");
+	// $("footer").load("footer.html");
+	get_con('js/list.json');
+
+	function get_con(file_name){
+		$.getJSON(file_name, function(data){
+			$("section").empty();
+
+			$.each(data, function(index, en){
+				var html ='<div class="col-3">';
+				html += '<a href="' + en['meme'] + '">';
+				html += '<div class="box">';
+				html += '<div class="imgbox">';
+				html += '<img src="' + en['pic'] + '">';
+				html += '</div>';
+				html += '<h3>' + en['title'] + '</h3>';
+				html += '</div>';
+				html += '</a>';
+				html += '</div>'
+
+				$("section").append(html);
+
+			});
+		});
+		return false;
+	}
+});
