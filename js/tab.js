ef542bb27c17deaa9efe75a7689f9ce6419cfad1
vanillamousse173 <vanillamousse173@gmail.com>
2019-01-06 02:37:15 +0800

    ahhhhh

--- js/tab.js
+++ js/tab.js
@@ -2,12 +2,12 @@
 
 $(document).ready(function() {
 		$(function(){
-    var $li = $('ul.tab-title li');
+    var $li = $('#c1,#c2,#c3,#c4');
         $($li. eq(0) .find('a').attr('href')).siblings('.tab-inner').hide();
     
         $li.click(function(){
             $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
-
+            $('#c1,#c2,#c3,#c4').addClass("ch_bg").siblings().removeClass("ch_bg");
         });
     });
     
