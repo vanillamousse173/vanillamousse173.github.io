9ccbefd675714ad3ded2e572c29711419e85e22b
vanillamousse173 <vanillamousse173@gmail.com>
2019-01-10 07:08:59 +0800

    scroll

--- js/home.js
+++ js/home.js
@@ -38,7 +38,7 @@ window.onscroll = function() {scrollFunction()};
         $('a[href*="#"]').on('click', function (e) {
             e.preventDefault();
             $('html, body').animate({
-                    scrollTop: $($(this).attr('href')).offset().top - 80
+                    scrollTop: $($(this).attr('href')).offset().top - 50
                 }, 500, 'linear');
         });
     });
