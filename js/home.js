523e238fee94354ff871bcb751e556faddc270ff
vanillamousse173 <vanillamousse173@gmail.com>
2019-01-09 23:03:56 +0800

    introdone

--- js/home.js
+++ js/home.js
@@ -33,50 +33,59 @@ window.onscroll = function() {scrollFunction()};
 		$(".hamburger").removeClass("is-active");
 	});
 
+	$(function(){
+        $('a[href*="#"]').on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({
+                    scrollTop: $($(this).attr('href')).offset().top - 50
+                }, 500, 'linear');
+        });
+    });
+
 //Click event to scroll to top
 //  $('#S2').offset().top
 
-$('#a1').click(function(){
-	$('html, body').animate({scrollTop : 0},1200);
-	return false;
-});
+// $('#a1').click(function(){
+// 	$('html, body').animate({scrollTop : 0},1200);
+// 	return false;
+// });
 
-$('#logo').click(function(){
-	$('html, body').animate({scrollTop : 0},1200);
-	return false;
-});
+// $('#logo').click(function(){
+// 	$('html, body').animate({scrollTop : 0},1200);
+// 	return false;
+// });
 
-$('#a2').click(function(){
-	$('html, body').animate({scrollTop : 670},1200);
-	return false;
-});
+// $('#a2').click(function(){
+// 	$('html, body').animate({scrollTop : 670},1200);
+// 	return false;
+// });
 
-$('#a3').click(function(){
-	$('html, body').animate({scrollTop : 1420},1200);
-	return false;
-});
+// $('#a3').click(function(){
+// 	$('html, body').animate({scrollTop : 1420},1200);
+// 	return false;
+// });
 
-$('#a4,#b4').click(function(){
-	$('html, body').animate({scrollTop : 2110},1200);
-	return false;
-});
+// $('#a4,#b4').click(function(){
+// 	$('html, body').animate({scrollTop : 2110},1200);
+// 	return false;
+// });
 
-$('#a5,#b5').click(function(){
-	$('html, body').animate({scrollTop : 2790},1200);
-	return false;
-});
+// $('#a5,#b5').click(function(){
+// 	$('html, body').animate({scrollTop : 2790},1200);
+// 	return false;
+// });
 
-$('#a6').click(function(){
-	$('html, body').animate({scrollTop : 3500},1200);
-	return false;
-});
+// $('#a6').click(function(){
+// 	$('html, body').animate({scrollTop : 3500},1200);
+// 	return false;
+// });
 
-$('#a7').click(function(){
-	$('html, body').animate({scrollTop : 4160},1200);
-	return false;
-});
+// $('#a7').click(function(){
+// 	$('html, body').animate({scrollTop : 4160},1200);
+// 	return false;
+// });
 
-$('.intro').eq(0).show();
+$('.intro').eq(49).show();
 
 $('.tip').hover(function(){
 	var _index = $(this).index();
