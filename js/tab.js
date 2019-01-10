91b8f5a8bc9da1da41d83aa220f5c79f0d5062e8
vanillamousse173 <vanillamousse173@gmail.com>
2019-01-10 22:27:26 +0800

    headerheight

--- js/tab.js
+++ js/tab.js
@@ -1,14 +1,19 @@
 // JavaScript Document
 
 $(document).ready(function() {
-		$(function(){
-            var $li = $('#c1,#c2,#c3,#c4');
-            $($li. eq(0) .find('a').attr('href')).siblings('.tab-inner').hide();
-       
-            $li.click(function(){
+    $(function(){
+        var $li = $('#c1,#c2,#c3,#c4');
+        $($li. eq(0) .find('a').attr('href')).siblings('.tab-inner').hide();
+    
+        $li.click(function(){
+        $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
+        });
+    
+    });
 
-            $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
-            // $(this).addClass("ch_bg").siblings().removeClass("ch_bg");
+    $(function(){
+        $("li").click(function(){
+            $(this).addClass("ch_bg").siblings().removeClass("ch_bg");
         });
     });
     
