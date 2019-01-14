8759c6b4205abe24ce06b53466efe5142a3e12f3
vanillamousse173 <vanillamousse173@gmail.com>
2019-01-14 18:39:21 +0800

    og

--- js/tab.js
+++ js/tab.js
@@ -1,20 +1,24 @@
 // JavaScript Document
 
 $(document).ready(function() {
-    $(function(){
-        var $li = $('#c1,#c2,#c3,#c4');
-        $($li. eq(0) .find('a').attr('href')).siblings('.tab-inner').hide();
-    
-        $li.click(function(){
+    var $li = $('#c1,#c2,#c3,#c4');
+    $($li. eq(0) .find('a').attr('href')).siblings('.tab-inner').hide();
+
+    $li.click(function(){
         $($(this).find('a'). attr ('href')).show().siblings ('.tab-inner').hide();
-        });
-    
     });
 
-    $(function(){
-        $("li>a").click(function(){
-            $(this).addClass("ch_bg").siblings().removeClass("ch_bg");
+    
+    // click li -> remove all .c_selected and add .c_selected on target li
+    document.querySelectorAll('.tab-title li').forEach(item => {
+        item.addEventListener('click', ()=> {
+            // clear all li .c_selected
+            document.querySelectorAll('.tab-title li').forEach(li => {
+                li.classList.remove('c_selected');
+            });
+            // add class on target li
+            item.classList.add('c_selected');
         });
     });
-    
+
 });
