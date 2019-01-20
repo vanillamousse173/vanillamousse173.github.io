20492908d78be76a11176263cef6ef2eca18b46b
vanillamousse173 <vanillamousse173@gmail.com>
2019-01-20 19:27:03 +0800

    weekly1

--- js/article.js
+++ js/article.js
@@ -3,11 +3,19 @@
 
 $(document).ready(function() {
 	
-// load
-	// $("header").load("header.html");
-	$("footer").load("footer.html");
+	// load
+		// $("header").load("header.html");
+		$("footer").load("footer.html");
 
 
-// iframe
+	// Click event to scroll to top
+	$(function(){
+		$('a[href*="#"]').on('click', function (e) {
+			e.preventDefault();
+			$('html, body').animate({
+					scrollTop: $($(this).attr('href')).offset().top - 120
+				}, 500, 'linear');
+		});
+	});
 
 });
